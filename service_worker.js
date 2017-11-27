ffbb4c852df984d7a9229d6b36b9f6b0b82f3f93
Kai Brabo <kaibrabo@gmail.com>
2017-11-26 23:49:03 -0800

    commited

---
+++ service_worker.js
@@ -0,0 +1,76 @@
+var cacheName = 'weatherPWA-step-6-1';
+
+var dataCacheName = 'weatherData-v1';
+
+var filesToCache = [
+    '/',
+    '/index.html',
+    '/scripts/app.js',
+    '/styles/inline.css',
+    '/images/clear.png',
+    '/images/cloudy-scattered-showers.png',
+    '/images/cloudy.png',
+    '/images/fog.png',
+    '/images/ic_add_white_24px.svg',
+    '/images/ic_refresh_white_24px.svg',
+    '/images/partly-cloudy.png',
+    '/images/rain.png',
+    '/images/scattered-showers.png',
+    '/images/sleet.png',
+    '/images/snow.png',
+    '/images/thunderstorm.png',
+    '/images/wind.png'
+];
+
+// Cache App shell
+self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+// clears cache of old data/
+self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (key !== cacheName && key !== dataCacheName) {
+                    console.log('[ServiceWorker] Removing old cache');
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    // loads service worker faster/loads previous data
+    return self.clients.claim();
+});
+
+// fetches data to update the app shell
+self.addEventListener('fetch', function(e) {
+    console.log('[ServiceWorker] Fetch', e.request.url);
+
+    var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
+
+    // service worker updates from network, then caches response
+    if (e.request.url.indexOf(dataUrl) > -1) {
+        e.respondWith(
+            caches.open(dataCacheName).then(function(cache) {
+                return fetch(e.request).then(function(response){
+                    cache.put(e.request.url, response.clone());
+                    return response;
+                });
+            })
+        );
+    } else {
+        e.respondWith(
+            caches.match(e.request).then(function(response) {
+                return response || fetch(e.request);
+            })
+        );
+    }
+});
